561e5f25469d94811047793e12e5fc07e52e92fd
Burak Ozen <burak@layer.co>
2021-09-10 12:12:51 +0200

    Id columns are added and put a comment about why we need an Id column

--- data/features/featureset1/featureset1_feature2.sql
+++ data/features/featureset1/featureset1_feature2.sql
@@ -3,9 +3,13 @@
     `featureset1_feature2` Feature
 */
 -- Note: It is a good practice to use the same names for your features in the code below and dataset.yaml file
+-- Ensure that you have the same ID column across all of your sql features. Layer joins your singular features using that ID column.
 
 
 SELECT
+ID_column,
+
 --Your SQL query goes here
+
 AS featureset1_feature2
 FROM your_table_name
--- data/features/featureset2/featureset2_feature2/feature.py
+++ data/features/featureset2/featureset2_feature2/feature.py
@@ -1,10 +1,13 @@
+# Ensure that you have the same ID column across all of your python features.
+# Layer joins your singular features using that ID column.
+
 from layer import Dataset
 
 
 def build_feature(layer_dataset: Dataset("layer_dataset_name_for_your_table")) -> Any:
     df = layer_dataset.to_pandas()
 
-    feature_data = df[["column1", "column2"]]
+    feature_data = df[["ID_column", "column_name"]]
 
     """
     Your python code goes here
